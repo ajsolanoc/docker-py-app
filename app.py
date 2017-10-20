6f85bea126bc16066a2026b1a1b94fa02d5d8b7b
Alberto Solano <albert.castellar@gmail.com>
2017-10-20 16:35:32 -0500

    latest

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from redis import Redis, RedisError
 import os
 import socket
+#import subprocess
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=1, socket_timeout=1)
@@ -17,9 +18,9 @@ def hello():
 
     html = "<h3>Hello from Docker <b>{name}</b>!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}<br/>" 
-           #"<b>This is a new version of my python app</b>"
-    return html.format(name=os.getenv("NAME", "CondorLabs"), hostname=socket.gethostname(), visits=visits)
-
+           "<b>Visits:</b> {visits}<br/>"\
+           #"<b>Container IP:</b> {ip}"
+    return html.format(name=os.getenv("NAME"), hostname=socket.gethostname(), visits=visits)
+    #return html.format(name=os.getenv("NAME", "CondorLabs"), hostname=socket.gethostname(), visits=visits, ip=subprocess.check_output(['bash', '-c', "/sbin/ip route|awk '/default/ { print $3 }'"]).decode('utf-8').strip())
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
